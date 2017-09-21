28a946daf4c6de497f262e95430b46a8b172a8c9
DanielS4L4SCR <luisda7781@hotmail.com>
2017-09-21 11:18:41 -0600

    Cambios

--- Proyect-BDll/CapaVista/Login.Designer.cs
+++ Proyect-BDll/CapaVista/Login.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(45, 91);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(117, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Nombre Servidor:";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click);
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Location = new System.Drawing.Point(168, 91);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(282, 29);
+            this.metroComboBox1.TabIndex = 1;
+            this.metroComboBox1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(306, 301);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(69, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Conectar";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(381, 301);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(69, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Cancelar";
+            this.metroButton2.UseSelectable = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 329);
+            this.ClientSize = new System.Drawing.Size(473, 347);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroLabel1);
             this.Name = "Login";
-            this.Text = "Login";
+            this.Text = "SQL MANAGER";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
     }
 }
