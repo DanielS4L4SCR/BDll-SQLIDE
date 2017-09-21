0bb7c338d4f44ebfef12d9103edefb24dcfbcea6
DanielS4L4SCR <luisda7781@hotmail.com>
2017-09-21 11:35:08 -0600

    Cambios

--- Proyect-BDll/CapaAccesoBD/Program.cs
+++ Proyect-BDll/CapaAccesoBD/Program.cs
@@ -16,7 +16,7 @@ namespace CapaAccesoBD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+           
         }
     }
 }
