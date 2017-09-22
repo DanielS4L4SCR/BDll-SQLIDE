566688095837649a3986884fced12984e04c2c3f
mariana9827 <nanaga98@hotmail.com>
2017-09-21 22:07:20 -0500

    Capa Negocios
    BD

---
+++ Proyect-BDll/CapaLogica/clsBaseDatos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class clsBaseDatos
+    {
+        public DataTable BaseDatos(String instanceName)
+        {
+            return new CapaAccesoBD.clsBaseDatos().BaseDatos(instanceName: instanceName);
+        }
+
+        public List<String> Intancias()
+        {
+            DataTable oDT = new CapaAccesoBD.clsBaseDatos().Instancias();
+            List<String> instance = new List<String>();
+            foreach (DataRow source in oDT.Rows)
+            {
+                string servername;
+                string instanceName = source["InstanceName"].ToString();
+
+                if (!string.IsNullOrEmpty(instanceName))
+                {
+                    servername = source["ServerName"] + "\\" + source["InstanceName"];
+                }
+                else
+                {
+                    servername = source["ServerName"].ToString();
+                }
+                instance.Add(servername);
+            }
+            return instance;
+
+        }
+    }
+}
+
