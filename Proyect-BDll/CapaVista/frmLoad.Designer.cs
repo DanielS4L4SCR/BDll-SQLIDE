605951f2e12f9fd436594eccd514afab3aca3238
DanielS4L4S2 <32578138+DanielS4L4S2@users.noreply.github.com>
2017-11-21 13:01:30 -0600

    Cambios

--- Proyect-BDll/CapaVista/frmLoad.Designer.cs
+++ Proyect-BDll/CapaVista/frmLoad.Designer.cs
@@ -54,7 +54,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(362, 119);
+            this.label2.Location = new System.Drawing.Point(382, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 16);
             this.label2.TabIndex = 5;
@@ -65,7 +65,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(27, 69);
+            this.label1.Location = new System.Drawing.Point(23, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(411, 50);
             this.label1.TabIndex = 4;
